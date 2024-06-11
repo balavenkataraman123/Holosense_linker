c3254d404e919bee689c3eaffc0374f32a603a78
Bala Venkataraman <38150148+balavenkataraman123@users.noreply.github.com>
2024-06-10 22:55:45 -0400

    Update holosense_backend.py

--- holosense_backend.py
+++ holosense_backend.py
@@ -24,8 +24,6 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
 
-dilation = 1
-
 camw = thisconf.camw
 camh = thisconf.camh
 htan = thisconf.htan
